13fcda053c147d6ef62e94ca8af55468d5427913
kubu4 <samuel.j.white@gmail.com>
2020-04-08 20:16:24 -0700

    update fasta filename

--- sbatch_scripts/20200407_cbai_busco_megan.sh
+++ sbatch_scripts/20200407_cbai_busco_megan.sh
@@ -45,7 +45,7 @@ prefix="${timestamp}.${species}"
 ## Input files and settings
 base_name="${prefix}.megan"
 busco_db=/gscratch/srlab/sam/data/databases/BUSCO/metazoa_odb9
-transcriptome_fasta=/gscratch/srlab/sam/data/C_bairdi/transcriptomes/20200406.C_bairdi.megan.Trinity.fasta
+transcriptome_fasta=/gscratch/srlab/sam/data/C_bairdi/transcriptomes/20200408.C_bairdi.megan.Trinity.fasta
 augustus_species=fly
 threads=28
 
