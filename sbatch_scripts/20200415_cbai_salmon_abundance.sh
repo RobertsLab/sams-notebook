0d0a7cbecf11331b40988c1d3ec8a0f65b45b256
kubu4 <samuel.j.white@gmail.com>
2020-04-15 08:58:13 -0700

    rm unused variables

--- sbatch_scripts/20200415_cbai_salmon_abundance.sh
+++ sbatch_scripts/20200415_cbai_salmon_abundance.sh
@@ -40,9 +40,6 @@ echo "${PATH}" | tr : \\n
 } >> system_path.log
 
 
-wd="$(pwd)"
-timestamp=$(date +%Y%m%d)
-species="cbai"
 threads=28
 
 
@@ -53,26 +50,16 @@ fasta_prefix="20200408.C_bairdi.megan.Trinity"
 trimmed_reads_dir="/gscratch/srlab/sam/data/C_bairdi/RNAseq"
 transcriptome_dir="/gscratch/srlab/sam/data/C_bairdi/transcriptomes"
 transcriptome="${transcriptome_dir}/${fasta_prefix}.fasta"
-fasta_seq_lengths="${transcriptome_dir}/${fasta_prefix}.fasta.seq_lens"
 
 trinotate_feature_map="${transcriptome_dir}/20200409.cbai.trinotate.annotation_feature_map.txt"
 gene_map="${transcriptome_dir}/${fasta_prefix}.fasta.gene_trans_map"
-salmon_gene_matrix="${salmon_out_dir}/salmon.gene.TMM.EXPR.matrix"
-salmon_iso_matrix="${salmon_out_dir}/salmon.isoform.TMM.EXPR.matrix"
-go_annotations="${transcriptome_dir}/20200409.cbai.trinotate.go_annotations.txt"
 
 
 # Standard output/error files
-diff_expr_stdout="diff_expr_stdout.txt"
-diff_expr_stderr="diff_expr_stderr.txt"
 matrix_stdout="matrix_stdout.txt"
 matrix_stderr="matrix_stderr.txt"
 salmon_stdout="salmon_stdout.txt"
 salmon_stderr="salmon_stderr.txt"
-tpm_length_stdout="tpm_length_stdout.txt"
-tpm_length_stderr="tpm_length_stderr.txt"
-trinity_DE_stdout="trinity_DE_stdout.txt"
-trinity_DE_stderr="trinity_DE_stderr.txt"
 
 
 #programs
@@ -80,12 +67,7 @@ trinity_home=/gscratch/srlab/programs/trinityrnaseq-v2.9.0
 trinity_annotate_matrix="${trinity_home}/Analysis/DifferentialExpression/rename_matrix_feature_identifiers.pl"
 trinity_abundance=${trinity_home}/util/align_and_estimate_abundance.pl
 trinity_matrix=${trinity_home}/util/abundance_estimates_to_matrix.pl
-trinity_DE=${trinity_home}/Analysis/DifferentialExpression/run_DE_analysis.pl
-diff_expr=${trinity_home}/Analysis/DifferentialExpression/analyze_diff_expr.pl
-trinity_tpm_length=${trinity_home}/util/misc/TPM_weighted_gene_length.py
 
-# Create directory/sample list for ${trinity_matrix} command
-trin_matrix_list=$(awk '{printf "%s%s", $2, "/quant.sf " }' "${samples}")
 
 # Rsync trimmed reads
 rsync \
@@ -106,7 +88,6 @@ done
 
 # Runs salmon and stranded library option
 ${trinity_abundance} \
---output_dir "${salmon_out_dir}" \
 --transcripts ${transcriptome} \
 --seqType fq \
 --left reads_1.fq \
@@ -125,8 +106,6 @@ ${trinity_matrix} \
 --est_method salmon \
 --gene_trans_map ${gene_map} \
 --out_prefix salmon \
---name_sample_by_basedir \
-"${trin_matrix_list}"
 1> ${matrix_stdout} \
 2> ${matrix_stderr}
 
