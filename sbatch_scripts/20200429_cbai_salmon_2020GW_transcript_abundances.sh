7a64cbc05c380e8e403a5ff1ad4413d8d2e846b3
kubu4 <samuel.j.white@gmail.com>
2020-04-29 10:32:09 -0700

    initial commit

---
+++ sbatch_scripts/20200429_cbai_salmon_2020GW_transcript_abundances.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=cbai_DEG_basic
+## Allocation Definition
+#SBATCH --account=coenv
+#SBATCH --partition=coenv
+## Resources
+## Nodes
+#SBATCH --nodes=1
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=10-00:00:00
+## Memory per node
+#SBATCH --mem=120G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samwhite@uw.edu
+## Specify the working directory for this job
+#SBATCH --chdir=/gscratch/scrubbed/samwhite/outputs/20200429_cbai_salmon_2020GW_transcript_abundances
+
+
+# Exit script if any command fails
+set -e
+
+# Load Python Mox module for Python module availability
+
+module load intel-python3_2017
+
+# Document programs in PATH (primarily for program version ID)
+
+{
+date
+echo ""
+echo "System PATH for $SLURM_JOB_ID"
+echo ""
+printf "%0.s-" {1..10}
+echo "${PATH}" | tr : \\n
+} >> system_path.log
+
+
+wd="$(pwd)"
+threads=28
+
+# Programs
+declare -A programs_array
+programs_array=([salmon]="/gscratch/srlab/programs/salmon-1.2.1_linux_x86_64/bin/salmon")
+
+## Designate input file locations
+fastq_dir="/gscratch/srlab/sam/data/C_bairdi/RNAseq/"
+salmon_index="/gscratch/srlab/sam/data/C_bairdi/transcriptomes/20200408.C_bairdi.megan.Trinity.fasta.salmon.idx"
+
+# Capture program options
+for program in "${!programs_array[@]}"
+do
+	{
+  echo "Program options for ${programs_array[program]}: "
+	echo ""
+	${programs_array[program]} quant --help
+	echo ""
+	echo ""
+	echo "----------------------------------------------"
+	echo ""
+	echo ""
+} &>> program_options.log || true
+done
