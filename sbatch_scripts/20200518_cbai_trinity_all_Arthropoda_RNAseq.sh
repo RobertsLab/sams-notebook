f1d56f937143c756a6de04f88333e4c322d811a8
kubu4 <samuel.j.white@gmail.com>
2020-05-18 11:31:10 -0700

    initial commit

---
+++ sbatch_scripts/20200518_cbai_trinity_all_Arthropoda_RNAseq.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=trinity_cbai
+## Allocation Definition
+#SBATCH --account=srlab
+#SBATCH --partition=srlab
+## Resources
+## Nodes
+#SBATCH --nodes=1
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=9-00:00:00
+## Memory per node
+#SBATCH --mem=500G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samwhite@uw.edu
+## Specify the working directory for this job
+#SBATCH --chdir=/gscratch/scrubbed/samwhite/outputs/20200518_cbai_trinity_all_Arthropoda_RNAseq
+
+### De novo transcriptome assembly of all Arthropoda-specific reads
+### Includes "descriptor_1" short-hand of: 2020-GW, 2020-UW, 2019, 2018.
+### See fastq.list.txt file for list of input files used for assembly.
+
+# Exit script if a command fails
+set -e
+
+# Load Python Mox module for Python module availability
+module load intel-python3_2017
+
+# Document programs in PATH (primarily for program version ID)
+{
+date
+echo ""
+echo "System PATH for $SLURM_JOB_ID"
+echo ""
+printf "%0.s-" {1..10}
+echo "${PATH}" | tr : \\n
+} >> system_path.log
+
+# User-defined variables
+reads_dir=/gscratch/srlab/sam/data/C_bairdi/RNAseq
+transcriptome_dir=/gscratch/srlab/sam/data/C_bairdi/transcriptomes
+threads=28
+assembly_stats=assembly_stats.txt
+fasta_name="cbai_transcriptome_v1.6.fasta"
+
+# Paths to programs
+trinity_dir="/gscratch/srlab/programs/trinityrnaseq-v2.9.0"
+samtools="/gscratch/srlab/programs/samtools-1.10/samtools"
+
+
+## Inititalize arrays
+R1_array=()
+R2_array=()
+
+# Variables for R1/R2 lists
+R1_list=""
+R2_list=""
+
+# Create array of fastq R1 files
+R1_array=("${reads_dir}"/*megan_R1*.fq)
+
+# Create array of fastq R2 files
+R2_array=("${reads_dir}"/*megan_R2*.fq)
+
+# Create list of fastq files used in analysis
+## Uses parameter substitution to strip leading path from filename
+for fastq in "${!R1_array[@]}"
+do
+  {
+    echo "${R1_array[${fastq}]##*/}"
+    echo "${R2_array[${fastq}]##*/}"
+  } >> fastq.list.txt
+done
+
+# Create comma-separated lists of FastQ reads
+R1_list=$(echo "${R1_array[@]}" | tr " " ",")
+R2_list=$(echo "${R2_array[@]}" | tr " " ",")
+
+
+# Run Trinity
+## Not running as "stranded", due to mix of library types
+${trinity_dir}/Trinity \
+--seqType fq \
+--max_memory 500G \
+--CPU ${threads} \
+--left "${R1_list}" \
+--right "${R2_list}"
+
+# Rename generic assembly FastA
+mv trinity_out_dir/Trinity.fasta trinity_out_dir/"${fasta_name}"
+
+# Assembly stats
+${trinity_dir}/util/TrinityStats.pl trinity_out_dir/"${fasta_name}" \
+> ${assembly_stats}
+
+# Create gene map files
+${trinity_dir}/util/support_scripts/get_Trinity_gene_to_trans_map.pl \
+trinity_out_dir/"${fasta_name}" \
+> trinity_out_dir/"${fasta_name}".gene_trans_map
+
+# Create sequence lengths file (used for differential gene expression)
+${trinity_dir}/util/misc/fasta_seq_length.pl \
+trinity_out_dir/"${fasta_name}" \
+> trinity_out_dir/"${fasta_name}".seq_lens
+
+# Create FastA index
+${samtools} faidx \
+trinity_out_dir/"${fasta_name}"
+
+# Copy files to transcriptome directory
+rsync -av \
+trinity_out_dir/"${fasta_name}"* \
+${transcriptome_dir}
+
+# Generate FastA MD5 checksum
+# See last line of SLURM output file
+cd trinity_out_dir
+md5sum trinity_out_dir/"${fasta_name}"
