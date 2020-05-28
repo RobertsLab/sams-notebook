830848484bc90320123b674b4a4c91ce47d3cff4
kubu4 <samuel.j.white@gmail.com>
2020-05-28 07:09:23 -0700

    initial commit

---
+++ bash_scripts/busco_comparison_plotting.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+## Script to run the BUSCO short summary comparison script
+## Will generate R code and resulting bar plot of all
+## summary files in the working directory
+
+### NOTE: A lot of this code is not needed (e.g. Augustus settings),
+### but was faster/easier to copy/paste and add what I needed
+### (primarily adding R path to config file)
+
+# Load Python Mox module for Python module availability
+module load intel-python3_2017
+
+# Load Open MPI module for parallel, multi-node processing
+module load icc_19-ompi_3.1.2
+
+# SegFault fix?
+export THREADS_DAEMON_MODEL=1
+
+# Document programs in PATH (primarily for program version ID)
+{
+date
+echo ""
+echo "System PATH for $SLURM_JOB_ID"
+echo ""
+printf "%0.s-" {1..10}
+echo "${PATH}" | tr : \\n
+} >> system_path.log
+
+
+## Input files and settings
+
+threads=28
+
+## Save working directory
+wd=$(pwd)
+
+
+## Set program paths
+augustus_bin=/gscratch/srlab/programs/Augustus-3.3.2/bin
+augustus_scripts=/gscratch/srlab/programs/Augustus-3.3.2/scripts
+blast_dir=/gscratch/srlab/programs/ncbi-blast-2.8.1+/bin/
+busco=/gscratch/srlab/programs/busco-v3/scripts/run_BUSCO.py
+busco_plot=/gscratch/srlab/programs/busco-v3/scripts/generate_plot.py
+hmm_dir=/gscratch/srlab/programs/hmmer-3.2.1/src/
+R_dir=/gscratch/srlab/programs/R-3.6.2/bin/
+
+## Augustus configs
+augustus_dir=${wd}/augustus
+augustus_config_dir=${augustus_dir}/config
+augustus_orig_config_dir=/gscratch/srlab/programs/Augustus-3.3.2/config
+
+## BUSCO configs
+busco_config_default=/gscratch/srlab/programs/busco-v3/config/config.ini.default
+busco_config_ini=${wd}/config.ini
+
+# Export BUSCO config file location
+export BUSCO_CONFIG_FILE="${busco_config_ini}"
+
+# Export Augustus variable
+export PATH="${augustus_bin}:$PATH"
+export PATH="${augustus_scripts}:$PATH"
+export AUGUSTUS_CONFIG_PATH="${augustus_config_dir}"
+
+
+# Copy BUSCO config file
+cp ${busco_config_default} "${busco_config_ini}"
+
+# Make Augustus directory if it doesn't exist
+if [ ! -d "${augustus_dir}" ]; then
+  mkdir --parents "${augustus_dir}"
+fi
+
+# Copy Augustus config directory
+cp --preserve -r ${augustus_orig_config_dir} "${augustus_dir}"
+
+# Edit BUSCO config file
+## Set paths to various programs
+### The use of the % symbol sets the delimiter sed uses for arguments.
+### Normally, the delimiter that most examples use is a slash "/".
+### But, we need to expand the variables into a full path with slashes, which screws up sed.
+### Thus, the use of % symbol instead (it could be any character that is NOT present in the expanded variable; doesn't have to be "%").
+sed -i "/^;cpu/ s/1/${threads}/" "${busco_config_ini}"
+sed -i "/^tblastn_path/ s%tblastn_path = /usr/bin/%path = ${blast_dir}%" "${busco_config_ini}"
+sed -i "/^makeblastdb_path/ s%makeblastdb_path = /usr/bin/%path = ${blast_dir}%" "${busco_config_ini}"
+sed -i "/^augustus_path/ s%augustus_path = /home/osboxes/BUSCOVM/augustus/augustus-3.2.2/bin/%path = ${augustus_bin}%" "${busco_config_ini}"
+sed -i "/^etraining_path/ s%etraining_path = /home/osboxes/BUSCOVM/augustus/augustus-3.2.2/bin/%path = ${augustus_bin}%" "${busco_config_ini}"
+sed -i "/^gff2gbSmallDNA_path/ s%gff2gbSmallDNA_path = /home/osboxes/BUSCOVM/augustus/augustus-3.2.2/scripts/%path = ${augustus_scripts}%" "${busco_config_ini}"
+sed -i "/^new_species_path/ s%new_species_path = /home/osboxes/BUSCOVM/augustus/augustus-3.2.2/scripts/%path = ${augustus_scripts}%" "${busco_config_ini}"
+sed -i "/^optimize_augustus_path/ s%optimize_augustus_path = /home/osboxes/BUSCOVM/augustus/augustus-3.2.2/scripts/%path = ${augustus_scripts}%" "${busco_config_ini}"
+sed -i "/^hmmsearch_path/ s%hmmsearch_path = /home/osboxes/BUSCOVM/hmmer/hmmer-3.1b2-linux-intel-ia32/binaries/%path = ${hmm_dir}%" "${busco_config_ini}"
+sed -i "/^/ s%Rscript_path = /usr/bin/%path = ${R_dir}%" "${busco_config_ini}"
+
+# Run BUSCO plotting script
+${busco_plot} -wd "${wd}"
