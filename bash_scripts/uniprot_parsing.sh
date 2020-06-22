615a1c32f98eea5839b63e89cb0a546107cdd140
kubu4 <samuel.j.white@gmail.com>
2020-06-22 11:41:30 -0700

    set up while loops, variables; set up http get request eval

--- bash_scripts/uniprot_parsing.sh
+++ bash_scripts/uniprot_parsing.sh
@@ -3,10 +3,52 @@
 ### Bash script for batch retrieval of Gene Ontology terms from uniprot.org from
 ### a list of UniProt accessions.
 
+# Initialize variables
+base_url=""
+descriptor=""
+error_count=0
+go_line=""
+response=""
+uniprot_file=""
+
+# Initialize arrays
+go_ids_array=()
+
+# Process UniProt accession list file.
 while read -r line
 do
-  entry=$(echo "${line}" | awk '{print $1}' )
-  if [[ "${entry}" == "ID" ]]; then
-    echo "${line}" | awk '{print $2}'
+  uniprot_file="${line}.txt"
+
+  # Record GET response code and download target file.
+  response=$(curl --write-out %{http_code} --silent --output "${uniprot_file}" https://www.uniprot.org/uniprot/${uniprot_file})
+
+  # Vefrify file was able to be retrieved, based on succesful HTTP server response code
+  if [[ "${response}" -eq 200 ]]; then
+
+    # Process downloaded UniProt accession text file.
+    while read -r line
+    do
+
+      # Get record line descriptor
+      descriptor=$(echo "${line}" | awk '{print $1}' )
+
+      # Capture second field for evaluation
+      go_line=$(echo "${line}" | awk '{print $2}')
+
+      # Print abbreviated protin name
+      if [[ "${descriptor}" == "ID" ]]; then
+        echo "${line}" | awk '{print $2}'
+      fi
+
+      # Append GO IDs to array
+      if [[ "${go_line}" == "GO;" ]]; then
+        echo "${line}" | awk '{print $3}' | tr --delete ";"
+      fi
+    done < "${uniprot_file}"
+
+  else
+    error_count=$((error_count+1))
+    printf "%s\n" "${line}" >> failed_accessions.txt
+    rm "${uniprot_file}"
   fi
 done < "$1"
