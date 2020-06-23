071f696b4949be7d7c22b8028ae73aed7bf543d4
kubu4 <samuel.j.white@gmail.com>
2020-06-23 10:52:03 -0700

    set up output print format; assing input line to new variable

--- bash_scripts/uniprot_parsing.sh
+++ bash_scripts/uniprot_parsing.sh
@@ -3,10 +3,14 @@
 ### Bash script for batch retrieval of Gene Ontology terms from uniprot.org from
 ### a list of UniProt accessions.
 
+input_file="$1"
+
 # Initialize variables
+accession=""
 base_url="https://www.uniprot.org/uniprot/"
 descriptor=""
 error_count=0
+go_id=""
 go_line=""
 response=""
 uniprot_file=""
@@ -17,10 +21,19 @@ go_ids_array=()
 # Process UniProt accession list file.
 while read -r line
 do
-  uniprot_file="${line}.txt"
+  accession="${line}"
+  go_ids_array=()
+  uniprot_file="${accession}.txt"
+
 
   # Record GET response code and download target file.
-  response=$(curl --write-out %{http_code} --silent --output "${uniprot_file}" https://www.uniprot.org/uniprot/"${uniprot_file}")
+  # --ciphers argument seems to be needed when using Ubuntu 20.04.
+  response=$(curl \
+  --write-out %{http_code}
+  --ciphers 'DEFAULT:@SECLEVEL=1' \
+  --silent \
+  --output "${uniprot_file}" \
+  "${base_url}${uniprot_file}")
 
   # Vefrify file was able to be retrieved, based on succesful HTTP server response code
   if [[ "${response}" -eq 200 ]]; then
@@ -35,7 +48,7 @@ do
       # Capture second field for evaluation
       go_line=$(echo "${line}" | awk '{print $2}')
 
-      # Print abbreviated protin name
+      # Print abbreviated protein name
       if [[ "${descriptor}" == "ID" ]]; then
         echo "${line}" | awk '{print $2}'
       fi
@@ -45,11 +58,19 @@ do
         go_id=$(echo "${line}" | awk '{print $3}')
         go_ids_array+=("${go_id}")
       fi
+
     done < "${uniprot_file}"
+
+
 
   else
     error_count=$((error_count+1))
-    printf "%s\n" "${line}" >> failed_accessions.txt
+    printf "%s\n" "${accession}" >> failed_accessions.txt
     rm "${uniprot_file}"
   fi
-done < "$1"
+
+  # Prints accession <tab> GOID1;GOID2;GOIDn; followed by newline
+  (IFS=; printf "%s\t%s" "${accession}" "${go_ids_array[*]}"; echo)
+
+
+done < "${input_file}"
