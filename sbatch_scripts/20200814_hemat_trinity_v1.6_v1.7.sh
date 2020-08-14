f47bdbaff857b48c067f2a8e80b259cd2e625a95
kubu4 <samuel.j.white@gmail.com>
2020-08-13 20:35:28 -0700

    initial commit

---
+++ sbatch_scripts/20200814_hemat_trinity_v1.6_v1.7.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=hemat_trinity_v1.6_v1.7
+## Allocation Definition
+#SBATCH --account=coenv
+#SBATCH --partition=coenv
+## Resources
+## Nodes
+#SBATCH --nodes=1
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=15-00:00:00
+## Memory per node
+#SBATCH --mem=500G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samwhite@uw.edu
+## Specify the working directory for this job
+#SBATCH --chdir=/gscratch/scrubbed/samwhite/
+
+
+###################################################################################
+# These variables need to be set by user
+
+# Assign Variables
+script_path=/gscratch/scrubbed/samwhite/20200814_hemat_trinity_v1.6_v1.7.sh
+reads_dir=/gscratch/srlab/sam/data/C_bairdi/RNAseq
+transcriptomes_dir=/gscratch/srlab/sam/data/Hematodinium/RNAseq
+threads=28
+max_mem=$(grep "#SBATCH --mem=" ${script_path} | awk -F [=G] '{print $2}')
+
+assembly_stats=assembly_stats.txt
+
+# Paths to programs
+trinity_dir="/gscratch/srlab/programs/trinityrnaseq-v2.9.0"
+samtools="/gscratch/srlab/programs/samtools-1.10/samtools"
+
+
+# Array of the various comparisons to evaluate
+# Each condition in each comparison should be separated by a "-"
+transcriptomes_array=(
+"${transcriptomes_dir}"/cbai_transcriptome_v1.6.fasta \
+"${transcriptomes_dir}"/cbai_transcriptome_v1.7.fasta
+)
+
+
+
+
+# Programs array
+declare -A programs_array
+programs_array=(
+[trinity]="${trinity_dir}/Trinity" \
+[samtools_faidx]="${samtools} faidx" \
+[trinity_stats]="${trinity_dir}/util/TrinityStats.pl"
+[detonate]="/gscratch/srlab/programs/detonate-1.11/rsem-eval/rsem-eval-calculate-score"
+)
+
+
+
+###################################################################################
+
+# Exit script if any command fails
+set -e
+
+# Load Python Mox module for Python module availability
+
+module load intel-python3_2017
+
+# Loop through each comparison
+for transcriptome in "${!transcriptomes_array[@]}"
+do
+
+  ## Inititalize arrays
+  R1_array=()
+  R2_array=()
+  reads_array=()
+
+  # Variables
+  R1_list=""
+  R2_list=""
+
+  transcriptome_name="${transcriptomes_array[$transcriptome]##*/}"
+
+
+  if [[ "${transcriptome_name}" == "cbai_transcriptome_v1.6.fasta" ]]; then
+
+    reads_array=("${reads_dir}"/*megan*.fq)
+
+    # Create array of fastq R1 files
+    R1_array=("${reads_dir}"/*megan*R1.fq)
+
+    # Create array of fastq R2 files
+    R2_array=("${reads_dir}"/*megan*R2.fq)
+
+  elif [[ "${transcriptome_name}" == "cbai_transcriptome_v1.7.fasta" ]]; then
+
+    reads_array=("${reads_dir}"/20200[145][13][189]*megan*.fq)
+
+    # Create array of fastq R1 files
+    R1_array=("${reads_dir}"/20200[145][13][189]*megan*R1.fq)
+
+    # Create array of fastq R2 files
+    R2_array=("${reads_dir}"/20200[145][13][189]*megan*R2.fq)
+
+  fi
+
+  # Create list of fastq files used in analysis
+  ## Uses parameter substitution to strip leading path from filename
+  printf "%s\n" "${reads_array[@]##*/}" >> "${transcriptome_name}".fastq.list.txt
+
+  # Create comma-separated lists of FastQ reads
+  R1_list=$(echo "${R1_array[@]}" | tr " " ",")
+  R2_list=$(echo "${R2_array[@]}" | tr " " ",")
+
+  # Determine transcript length
+  ${programs_array[detonate_trans_length]} \
+  "${transcriptomes_array[$transcriptome]}" \
+  "${rsem_eval_dist_mean_sd}"
+
+
+  # Capture FastA checksums for verification
+  echo "Generating checksum for ${transcriptome_name}"
+  md5sum "${transcriptomes_array[transcriptome]}" >> fasta.checksums.md5
+  echo "Finished generating checksum for ${transcriptome_name}"
+  echo ""
+
+done
+
+# Document programs in PATH (primarily for program version ID)
+{
+date
+echo ""
+echo "System PATH for $SLURM_JOB_ID"
+echo ""
+printf "%0.s-" {1..10}
+echo "${PATH}" | tr : \\n
+} >> system_path.log
+
+# Capture program options
+for program in "${!programs_array[@]}"
+do
+	{
+  echo "Program options for ${program}: "
+	echo ""
+	${programs_array[$program]} --help
+	echo ""
+	echo ""
+	echo "----------------------------------------------"
+	echo ""
+	echo ""
+} &>> program_options.log || true
+done
