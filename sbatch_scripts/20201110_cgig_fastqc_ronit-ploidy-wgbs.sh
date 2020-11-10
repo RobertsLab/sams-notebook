f34729e3ecadc827ed517c6aafabe79f3aedaebb
kubu4 <samuel.j.white@gmail.com>
2020-11-10 10:37:52 -0800

    initial commit

---
+++ sbatch_scripts/20201110_cgig_fastqc_ronit-ploidy-wgbs.sh
@@ -0,0 +1,93 @@
+
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=20201110_cgig_fastqc_ronit-ploidy-wgbs
+## Allocation Definition
+#SBATCH --account=coenv
+#SBATCH --partition=coenv
+## Resources
+## Nodes
+#SBATCH --nodes=1
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=10-00:00:00
+## Memory per node
+#SBATCH --mem=120G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samwhite@uw.edu
+## Specify the working directory for this job
+#SBATCH --chdir=/gscratch/scrubbed/samwhite/outputs/20201110_cgig_fastqc_ronit-ploidy-wgbs
+### Roberto's C.gigas WGBS trimming using fastp.
+
+
+###################################################################################
+# These variables need to be set by user
+
+# Set number of CPUs to use
+threads=28
+
+# Input/output files
+checksums=fastq_checksums.md5
+raw_reads_dir=/gscratch/srlab/sam/data/C_gigas/wgbs/
+
+# Paths to programs
+fastqc=/gscratch/srlab/programs/fastqc_v0.11.9/fastqc
+multiqc=/gscratch/srlab/programs/anaconda3/bin/multiqc
+
+
+# Programs associative array
+declare -A programs_array
+programs_array=(
+[fastqc]="${fastqc}" \
+[multiqc]="${multiqc}"
+)
+
+###################################################################################
+
+# Exit script if any command fails
+set -e
+
+# Load Python Mox module for Python module availability
+module load intel-python3_2017
+
+
+
+
+
+# Capture program options
+for program in "${!programs_array[@]}"
+do
+	{
+  echo "Program options for ${program}: "
+	echo ""
+  # Handle samtools help menus
+  if [[ "${program}" == "samtools_index" ]] \
+  || [[ "${program}" == "samtools_sort" ]] \
+  || [[ "${program}" == "samtools_view" ]]
+  then
+    ${programs_array[$program]}
+  fi
+	${programs_array[$program]} -h
+	echo ""
+	echo ""
+	echo "----------------------------------------------"
+	echo ""
+	echo ""
+} &>> program_options.log || true
+
+  # If MultiQC is in programs_array, copy the config file to this directory.
+  if [[ "${program}" == "multiqc" ]]; then
+  	cp --preserve ~/.multiqc_config.yaml .
+  fi
+done
+
+
+# Document programs in PATH (primarily for program version ID)
+{
+date
+echo ""
+echo "System PATH for $SLURM_JOB_ID"
+echo ""
+printf "%0.s-" {1..10}
+echo "${PATH}" | tr : \\n
+} >> system_path.log
