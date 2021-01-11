ce2358a94a80e474dc11eb576295de845bed19da
kubu4 <samuel.j.white@gmail.com>
2021-01-11 11:48:10 -0800

    initial commit

--- bash_scripts/notebook_backups_github.sh
+++ bash_scripts/notebook_backups_github.sh
@@ -2,3 +2,27 @@
 
 # A script to create mirrored versions of Roberts Lab
 # GitHub-based notebooks for offline viewing and archiving.
+
+
+
+## Identify files >100MB to avoid downloading them.
+
+# Set field separator to newling to process output from find
+IFS=$'\n'
+
+# Initialize array
+array=()
+
+for item in $(find . -type f -size +100M)
+do
+  file=${item##*/}
+  echo $file
+  array+=($file)
+done
+
+# Restore field separator to default (space)
+unset IFS
+
+# Create comma-separated list for use in wget
+# Use "${joined%,}" later on to remove trailing comma after last element
+printf -v joined '%s,' "${array[@]}"
