def8f1bcb772303021b36c844191a52550b88cc7
kubu4 <samuel.j.white@gmail.com>
2021-04-02 08:36:53 -0700

    initial commit

---
+++ sbatch_scripts/20210406_pgen_blobtools_Panopea-generosa-v1.0.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=20210401_pgen_fastp_10x-genomics
+## Allocation Definition
+#SBATCH --account=coenv
+#SBATCH --partition=coenv
+## Resources
+## Nodes
+#SBATCH --nodes=1
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=10-00:00:00
+## Memory per node
+#SBATCH --mem=120G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samwhite@uw.edu
+## Specify the working directory for this job
+#SBATCH --chdir=/gscratch/scrubbed/samwhite/outputs/20210401_pgen_fastp_10x-genomics
+
+
+### Fastp 10x Genomics data used for P.generosa genome assembly by Phase Genomics.
+### In preparation for use in BlobToolKit
+
+### Expects input filenames to be in format: *.fastq.gz
+
+
+###################################################################################
+# These variables need to be set by user
+
+## Assign Variables
+
+# Set number of CPUs to use
+threads=40
+
+# Input/output files
+trimmed_reads_dir=/gscratch/scrubbed/samwhite/outputs/20210401_pgen_fastp_10x-genomics/
+fastq_checksums=fastq_checksums.md5
+
+
+# Programs associative array
+declare -A programs_array
+programs_array=(
+[fastp]="${fastp}" \
+[multiqc]="${multiqc}"
+)
+
+
+###################################################################################
+
+# Exit script if any command fails
+set -e
+
+
+
+
+###################################################################################
+
+# Capture program options
+echo "Logging program options..."
+for program in "${!programs_array[@]}"
+do
+	{
+  echo "Program options for ${program}: "
+	echo ""
+  # Handle samtools help menus
+  if [[ "${program}" == "samtools_index" ]] \
+  || [[ "${program}" == "samtools_sort" ]] \
+  || [[ "${program}" == "samtools_view" ]]
+  then
+    ${programs_array[$program]}
+
+  # Handle DIAMOND BLAST menu
+  elif [[ "${program}" == "diamond" ]]; then
+    ${programs_array[$program]} help
+
+  # Handle NCBI BLASTx menu
+  elif [[ "${program}" == "blastx" ]]; then
+    ${programs_array[$program]} -help
+  fi
+	${programs_array[$program]} -h
+	echo ""
+	echo ""
+	echo "----------------------------------------------"
+	echo ""
+	echo ""
+} &>> program_options.log || true
+
+  # If MultiQC is in programs_array, copy the config file to this directory.
+  if [[ "${program}" == "multiqc" ]]; then
+  	cp --preserve ~/.multiqc_config.yaml multiqc_config.yaml
+  fi
+done
+
+# Document programs in PATH (primarily for program version ID)
+{
+  date
+  echo ""
+  echo "System PATH for $SLURM_JOB_ID"
+  echo ""
+  printf "%0.s-" {1..10}
+  echo "${PATH}" | tr : \\n
+} >> system_path.log
+
+echo "Finished logging system PATH"
