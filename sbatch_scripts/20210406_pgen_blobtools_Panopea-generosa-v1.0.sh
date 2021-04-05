17588ab7eb8b1ddb3aa5b95621b8fe4a1e2592db
kubu4 <samuel.j.white@gmail.com>
2021-04-05 11:43:05 -0700

    add config yaml printf statements

--- sbatch_scripts/20210406_pgen_blobtools_Panopea-generosa-v1.0.sh
+++ sbatch_scripts/20210406_pgen_blobtools_Panopea-generosa-v1.0.sh
@@ -29,18 +29,45 @@
 
 ## Assign Variables
 
+# Pipeline options
+
+evalue="1e-25"
+ncbi_tax_id=1049056
+species="Panopea generosa"
+root=1
+
 # Set number of CPUs to use
 threads=40
 
 # Input/output files
-trimmed_reads_dir=/gscratch/scrubbed/samwhite/outputs/20210401_pgen_fastp_10x-genomics/
+genome_fasta=/gscratch/srlab/sam/data/P_generosa/genomes/Panopea-generosa-v1.0.fa
 fastq_checksums=fastq_checksums.md5
+trimmed_reads_dir=/gscratch/scrubbed/samwhite/outputs/20210401_pgen_fastp_10x-genomics/
+config="${genome_fasta##*/}_btk.yaml"
 
+# Programs
+blobtools2=/gscratch/srlab/programs/blobtoolkit/blobtools2
+
+# Databases
+
+## BUSCO lineage database directory
+busco_dbs=/gscratch/srlab/sam/data/databases/BUSCO
+
+## Blobtools NCBI taxonomy database directory
+btk_ncbi_tax_dir=/gscratch/srlab/blastdbs/20210401_ncbi_taxonomy
+
+## NCBI nt database dir
+ncbi_db=/gscratch/srlab/blastdbs/20210401_ncbi_nt
+ncbi_db_name="nt"
+
+## Uniprot DIAMOND database dir
+uniprot_db=/gscratch/srlab/blastdbs/20210401_uniprot_btk
+uniprot_db_name=reference_proteomes
 
 # Programs associative array
 declare -A programs_array
 programs_array=(
-[fastp]="${fastp}" \
+[blobltools2]="${blobtools2}" \
 [multiqc]="${multiqc}"
 )
 
@@ -77,6 +104,45 @@ do
   cat ${fastq} >> reads_R2.fq.gz
   echo "Finished concatenating ${fastq} to reads_R2.fq.gz"
 done
+
+
+# Create BTK config YAML
+
+{
+  printf "%s\n" "assembly:"
+  printf "%2s%s\n" "" "accession: draft"
+  printf "%2s%s\n" "" "level: scaffold"
+  printf "%2s%s\n" "" "prefix: Panopea-generosa-v1.0"
+  printf "%s\n" "busco:"
+  printf "%2s%s\n" "" "lineages:"
+  printf "%4s%s\n" "" "- eukaryota_odb9" "" "- metazoa_odb9"
+  printf "%2s%s\n" "" "lineage_dir: ${busco_dbs}"
+  printf "%s\n" "reads:"
+  printf "%2s%s\n" "paired:"
+  printf "%4s%s\n" "" "-"
+  printf "%6s%s\n" "" "- reads" "" "- ILLUMINA"
+  printf "%s\n" "settings:"
+  printf "%2s%s\n" "" "blobtools2_path: ${blobtools2}"
+  printf "%2s%s\n" "" "taxonomy: ${btk_ncbi_tax_dir}"
+  printf "%2s%s\n" "" "/tmp"
+  printf "%2s%s\n" "" "blast_chunk: 100000"
+  printf "%2s%s\n" "" "blast_max_chunks: 10"
+  printf "%2s%s\n" "" "blast_overlap: 500"
+  printf "%2s%s\n" "" "chunk: 1000000"
+  printf "%s\n" "similarity:"
+  printf "%2s%s\n" "" "defaults:"
+  printf "%4s%s\n" "" "evalue: ${evalue}" "" "max_target_seqs: 10" "" "root: ${root}" "" "mask_ids: []"
+  printf "%2s%s\n" "" "databases:"
+  printf "%4s%s\n" "" "-"
+  printf "%6s%s\n" "" "local: ${ncbi_db}" "" "name: ${ncbi_db_name}" "" "source: ncbi" "" "tools: blast" "" "type: nucl"
+  printf "%4s%s\n" "" "-"
+  printf "%6s%s\n" "" "local: ${uniprot_db}" "" "max_target_seqs: 1" "" "name: ${uniprot_db_name}" "" "source: uniprot" "" "tools: diamond" "" "type: prot"
+  printf "%2s%s\n" "" "taxrule: bestsumorder"
+  printf "%s\n" "taxon:"
+  printf "%2s%s\n" "" "taxid: ${ncbi_tax_id}" "" "name: ${species}"
+  printf "%s\n" "keep_intermediates: true"
+} >> ${config}
+
 
 ###################################################################################
 
