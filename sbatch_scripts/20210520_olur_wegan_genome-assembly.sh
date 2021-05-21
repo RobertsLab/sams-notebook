9e1cb2ad24388c99b605c505db02c79173b7e6f6
kubu4 <samuel.j.white@gmail.com>
2021-05-21 05:11:21 -0700

    add commands to run, comments

--- sbatch_scripts/20210520_olur_wegan_genome-assembly.sh
+++ sbatch_scripts/20210520_olur_wegan_genome-assembly.sh
@@ -17,19 +17,24 @@
 ## Specify the working directory for this job
 #SBATCH --chdir=/gscratch/scrubbed/samwhite/outputs/20210520_olur_wegan_genome-assembly
 
+# Script to attempt hybrid genome assembly of O.lurida Illumina and PacBio data using Wengan
+# Container built according to instructions here:
+# https://github.com/adigenova/wengan
+
 
 ###################################################################################
 # These variables need to be set by user
-
-
 
 # Set number of CPUs to use
 threads=28
 
 # Input/output files
 fastq_checksums=fastq_checksums.md5
-trimmed_reads_dir=/gscratch/scrubbed/samwhite/outputs/20210401_pgen_fastp_10x-genomics
+trimmed_reads_dir=/gscratch/scrubbed/samwhite/outputs/20210518_olur_fastp_bgi
+pacbio_reads_dir=/gscratch/srlab/sam/data/O_lurida/DNAseq
 
+# Set genome size in Mbp
+genome_size=1900
 
 
 # Programs associative array
@@ -42,12 +47,25 @@ programs_array=()
 # Exit script if any command fails
 set -e
 
+# Load Singularity module
+module load singularity
 
-# Generate checksum for "new" FastA
-md5sum ${genome_fasta} > genome_fasta.md5
+# Seeting for Singularity container
+export TMPDIR=/tmp
+
+#location of wengan in the container
+WENGAN=/wengan/wengan-v0.2-bin-Linux/wengan.pl
+
+# Container name
+CONTAINER=wengan_v0.2.sif
+
+
+# Copy container
+rsync -avp /gscratch/srlab/programs/singularity_containers/$CONTAINER .
+
 
 # Concatenate all R1 reads
-for fastq in "${trimmed_reads_dir}"/*R1*.fq.gz
+for fastq in "${trimmed_reads_dir}"/*_1*.fq.gz
 do
   echo ""
   echo "Generating checksum for ${fastq}"
@@ -61,7 +79,7 @@ do
 done
 
 # Concatenate all R2 reads
-for fastq in "${trimmed_reads_dir}"/*R2*.fq.gz
+for fastq in "${trimmed_reads_dir}"/*_2*.fq.gz
 do
   echo ""
   echo "Generating checksum for ${fastq}"
@@ -74,7 +92,33 @@ do
   echo "Finished concatenating ${fastq} to reads_2.fastq.gz"
 done
 
+# Concatenate all PacBio reads
+for fastq in "${pacbio_reads_dir}"/*.fastq.gz
+do
+  echo ""
+  echo "Generating checksum for ${fastq}"
+  md5sum "${fastq}" >> ${fastq_checksums}
+  echo "Checksum generated for ${fastq}."
 
+  echo ""
+  echo "Concatenating ${fastq} to pacbio.clr.fastq.gz"
+  cat "${fastq}" >> pacbio.clr.fastq.gz
+  echo "Finished concatenating ${fastq} to pacbio.clr.fastq.gz"
+done
+
+
+#run WenganM with singularity exec
+singularity exec $CONTAINER perl ${WENGAN} \
+ -x pacraw \
+ -a M \
+ -s reads_1.fastq.gz,reads_2.fastq.gz \
+ -l pacbio.clr.fastq.gz \
+ -p Olur_v090 \
+ -t ${threads} \
+ -g ${genome_size}
+
+ # Remove concatenated reads files
+ rm *.fastq.gz
 
 ###################################################################################
 
