cd05d61430269541c9610d18515eefc829dad4db
kubu4 <samuel.j.white@gmail.com>
2022-03-17 07:03:19 -0700

    add gzip & md5sum to uncompressed array loop

--- sbatch_scripts/20220315-pgen-nextflow_rnaseq-tissues.sh
+++ sbatch_scripts/20220315-pgen-nextflow_rnaseq-tissues.sh
@@ -26,6 +26,9 @@
 
 # These variables need to be set by user
 
+# NF Core RNAseq workflow directory
+nf-core_rnaseq=""
+
 # RNAseq FastQs directory
 reads_dir=/gscratch/srlab/sam/data/P_generosa/RNAseq
 
@@ -75,52 +78,54 @@ R2_uncompressed_array=("${reads_dir}"/*_2.fastq)
 # Check array size to confirm it has all expected samples
 # Exit if mismatch
 if [[ "${#R1_uncompressed_array[@]}" != "${#R2_uncompressed_array[@]}" ]]
-  then
-    echo ""
-    echo "Uncompressed array sizes don't match."
-    echo "Confirm all expected FastQs are present in ${reads_dir}"
-    echo ""
+then
+  echo ""
+  echo "Uncompressed array sizes don't match."
+  echo "Confirm all expected FastQs are present in ${reads_dir}"
+  echo ""
 
-    exit
+  exit
 fi
 
 # Create list of original uncompressed fastq files
 ## Uses parameter substitution to strip leading path from filename
 for fastq in "${!R1_uncompressed_array[@]}"
 do
-  {
-    md5sum "${R1_uncompressed_array[${fastq}]}"
-    md5sum "${R2_uncompressed_array[${fastq}]}"
-  } >> uncompressed_fastqs-"${SLURM_JOB_ID}".md5
-done
+  # Strip leading path
+	no_path=$(echo "${R1_uncompressed_array[${fastq}]##*/}")
 
-
-# Compress FastQs
-## Required for Nextflow Core RNAseq pipeline
-for fastq in ${fastq_dir}/*.fastq
-do
   # Grab SRA name
-  sra=$(awk -F "_" '{print $1}')
+  sra=$(echo "${no_path}" | awk -F "_" '{print $1}')
 
   # Only gzip matching FastQs
+  # Only generate MD5 checksums for matching FastQs
   if [[ "${sra}" == "SRR12218868" ]] \
-  || [[ "${sra}" == "SRR12218869" ]] \
-  || [[ "${sra}" == "SRR12226692" ]] \
-  || [[ "${sra}" == "SRR12218870" ]] \
-  || [[ "${sra}" == "SRR12226693" ]] \
-  || [[ "${sra}" == "SRR12207404" ]] \
-  || [[ "${sra}" == "SRR12207405" ]] \
-  || [[ "${sra}" == "SRR12227930" ]] \
-  || [[ "${sra}" == "SRR12207406" ]] \
-  || [[ "${sra}" == "SRR12207407" ]] \
-  || [[ "${sra}" == "SRR12227931" ]] \
-  || [[ "${sra}" == "SRR12212519" ]] \
-  || [[ "${sra}" == "SRR12227929" ]] \
-  || [[ "${sra}" == "SRR8788211" ]]
+    || [[ "${sra}" == "SRR12218869" ]] \
+    || [[ "${sra}" == "SRR12226692" ]] \
+    || [[ "${sra}" == "SRR12218870" ]] \
+    || [[ "${sra}" == "SRR12226693" ]] \
+    || [[ "${sra}" == "SRR12207404" ]] \
+    || [[ "${sra}" == "SRR12207405" ]] \
+    || [[ "${sra}" == "SRR12227930" ]] \
+    || [[ "${sra}" == "SRR12207406" ]] \
+    || [[ "${sra}" == "SRR12207407" ]] \
+    || [[ "${sra}" == "SRR12227931" ]] \
+    || [[ "${sra}" == "SRR12212519" ]] \
+    || [[ "${sra}" == "SRR12227929" ]] \
+    || [[ "${sra}" == "SRR8788211" ]]
   then
-    gzip "${fastq}"
+    # Gzip FastQs; NF Core RNAseq requires gzipped FastQs as inputs
+    gzip "${R1_uncompressed_array[${fastq}]}"
+    gzip "${R2_uncompressed_array[${fastq}]}"
+
+    # Generate MD5 checksums of uncompressed FastQs
+    {
+      md5sum "${R1_uncompressed_array[${fastq}]}"
+      md5sum "${R2_uncompressed_array[${fastq}]}"
+    } >> uncompressed_fastqs-"${SLURM_JOB_ID}".md5
   fi
 done
+
 
 # Create array of fastq R1 files
 # Set filename pattern
@@ -130,7 +135,7 @@ R1_array=("${reads_dir}"/*_1.fastq.gz)
 # Set filename pattern
 R2_array=("${reads_dir}"/*_2.fastq.gz)
 
-# Check array size to confirm it has all expected samples
+# Check array sizes to confirm they are same size
 # Exit if mismatch
 if [[ "${#R1_array[@]}" != "${#R2_array[@]}" ]]
   then
@@ -146,71 +151,71 @@ fi
 ## Uses parameter substitution to strip leading path from filename
 for fastq in "${!R1_array[@]}"
 do
+  # Generate MD5 checksums for compressed FastQs used in NF Core RNAseq analysis
   {
     md5sum "${R1_array[${fastq}]}"
     md5sum "${R2_array[${fastq}]}"
   } >> input_fastqs-"${SLURM_JOB_ID}".md5
 
+  # Strip leading path
+	no_path=$(echo "${R1_array[${fastq}]##*/}")
+
   # Grab SRA name
-  sra=$(awk -F "_" '{print $1}')
+  sra=$(echo "${no_path}" | awk -F "_" '{print $1}')
 
   # Set tissue type
   if [[ "${sra}" == "SRR12218868" ]]
-    then
-      tissue="heart"
+  then
+    tissue="heart"
 
-      # Add to NF Core RNAseq sample sheet
-      printf "%s,%s,%s,%s\n" "${tissue}" "${R1_array[${fastq}]}" "${R2_array[${fastq}]}" "reverse" \
-}     >> sample_sheet-"${SLURM_JOB_ID}".csv
+    # Add to NF Core RNAseq sample sheet
+    printf "%s,%s,%s,%s\n" "${tissue}" "${R1_array[${fastq}]}" "${R2_array[${fastq}]}" "reverse" \
+    >> sample_sheet-"${SLURM_JOB_ID}".csv
 
   elif [[ "${sra}" == "SRR12218869" ]] \
-  || [[ "${sra}" == "SRR12226692" ]]
+    || [[ "${sra}" == "SRR12226692" ]]
 
-    then
-      tissue="gonad"
+  then
+    tissue="gonad"
 
-      # Add to NF Core RNAseq sample sheet
-      printf "%s,%s,%s,%s\n" "${tissue}" "${R1_array[${fastq}]}" "${R2_array[${fastq}]}" "reverse" \
-      >> sample_sheet-"${SLURM_JOB_ID}".csv
+    # Add to NF Core RNAseq sample sheet
+    printf "%s,%s,%s,%s\n" "${tissue}" "${R1_array[${fastq}]}" "${R2_array[${fastq}]}" "reverse" \
+    >> sample_sheet-"${SLURM_JOB_ID}".csv
 
   elif [[ "${sra}" == "SRR12218870" ]] \
-  || [[ "${sra}" == "SRR12226693" ]]
-    then
-      tissue="ctenidia"
+    || [[ "${sra}" == "SRR12226693" ]]
+  then
+    tissue="ctenidia"
 
-      # Add to NF Core RNAseq sample sheet
-      printf "%s,%s,%s,%s\n" "${tissue}" "${R1_array[${fastq}]}" "${R2_array[${fastq}]}" "reverse" \
-      >> sample_sheet-"${SLURM_JOB_ID}".csv
+    # Add to NF Core RNAseq sample sheet
+    printf "%s,%s,%s,%s\n" "${tissue}" "${R1_array[${fastq}]}" "${R2_array[${fastq}]}" "reverse" \
+    >> sample_sheet-"${SLURM_JOB_ID}".csv
 
   elif [[ "${sra}" == "SRR12207404" ]] \
-  || [[ "${sra}" == "SRR12207405" ]] \
-  || [[ "${sra}" == "SRR12227930" ]] \
-  || [[ "${sra}" == "SRR12207406" ]] \
-  || [[ "${sra}" == "SRR12207407" ]] \
-  || [[ "${sra}" == "SRR12227931" ]]
-    then
-      tissue="juvenile"
+    || [[ "${sra}" == "SRR12207405" ]] \
+    || [[ "${sra}" == "SRR12227930" ]] \
+    || [[ "${sra}" == "SRR12207406" ]] \
+    || [[ "${sra}" == "SRR12207407" ]] \
+    || [[ "${sra}" == "SRR12227931" ]]
+  then
+    tissue="juvenile"
 
-      # Add to NF Core RNAseq sample sheet
-      printf "%s,%s,%s,%s\n" "${tissue}" "${R1_array[${fastq}]}" "${R2_array[${fastq}]}" "reverse" \
-      >> sample_sheet-"${SLURM_JOB_ID}".csv
+    # Add to NF Core RNAseq sample sheet
+    printf "%s,%s,%s,%s\n" "${tissue}" "${R1_array[${fastq}]}" "${R2_array[${fastq}]}" "reverse" \
+    >> sample_sheet-"${SLURM_JOB_ID}".csv
 
   elif [[ "${sra}" == "SRR12212519" ]] \
-  || [[ "${sra}" == "SRR12227929" ]] \
-  || [[ "${sra}" == "SRR8788211" ]]
-    then
-      tissue="larvae"
+    || [[ "${sra}" == "SRR12227929" ]] \
+    || [[ "${sra}" == "SRR8788211" ]]
+  then
+    tissue="larvae"
 
-      # Add to NF Core RNAseq sample sheet
-      printf "%s,%s,%s,%s\n" "${tissue}" "${R1_array[${fastq}]}" "${R2_array[${fastq}]}" "reverse" \
-      >> sample_sheet-"${SLURM_JOB_ID}".csv
+    # Add to NF Core RNAseq sample sheet
+    printf "%s,%s,%s,%s\n" "${tissue}" "${R1_array[${fastq}]}" "${R2_array[${fastq}]}" "reverse" \
+    >> sample_sheet-"${SLURM_JOB_ID}".csv
   fi
 
-# Create NF Core RNAseq sample sheet
-{
-  printf "%s\n" "${sample_sheet_header}"
-  printf "%s,%s,%s,%s\n" "${tissue}" "${R1_array[${fastq}]}" "${R2_array[${fastq}]}" "reverse"
-} >> sample_sheet-"${SLURM_JOB_ID}".csv
 done
 
 
+# Run NF Core RNAseq workflow
