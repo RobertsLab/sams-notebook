9ee2de63218dc0531f133953d02dedd416488a43
kubu4 <samuel.j.white@gmail.com>
2023-06-28 14:35:03 -0700

    change Amil fasta name to match shortstack req

--- sbatch_scripts/20230628-E5_coral-ShortStack-sRNAseq.sh
+++ sbatch_scripts/20230628-E5_coral-ShortStack-sRNAseq.sh
@@ -64,7 +64,7 @@ programs_array=(
 # Genomes associative array
 declare -A genomes_array
 genomes_array=(
-    [A_pulchra]="/gscratch/srlab/sam/data/A_millepora/genomes/GCF_013753865.1_Amil_v2.1_genomic.fna" \
+    [A_pulchra]="/gscratch/srlab/sam/data/A_millepora/genomes/GCF_013753865.1_Amil_v2.1_genomic.fasta" \
     [P_meandrina]="/gscratch/srlab/sam/data/P_meandrina/genomes/Pocillopora_meandrina_HIv1.assembly.fasta"
     )
 
