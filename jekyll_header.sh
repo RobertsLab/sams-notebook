724e833862f4b54477d95b54d404c386f2693194
kubu4 <samuel.j.white@gmail.com>
2023-09-27 17:06:15 -0700

    something weird with jekyll_header.sh

--- jekyll_header.sh
+++ jekyll_header.sh
@@ -1,49 +1,52 @@
 #!/usr/bin/env bash
 
 # This script is designed to create a markdown file that
-# generates a formatted Jekyll header. It prompts the user
+# generates a formatted Quarto YAML header. It prompts the user
 # for a post title and that title is utilized in the name of
-# the markdown file that is created.
+# the subsequent directory that is created.
 
-# It also prompts the user for tags/categories and launches the Atom text editing software after this information is provided.
+# It also prompts the user for categories and launches the Atom text editing software after this information is provided.
 
-# Tag/category formatting is specifically for the Basically Basic theme, which requires a Jekyll array format.
-# Space-separated formatting for tags/categories are available in this git commit:
-# https://github.com/kubu4/Scripts/blob/3712cdf474a8008dd4d314418f19f5579e360466/bash/jekyll_header.sh
+# The index.qmd file is also put in draft mode. This prevents rendering until removed or set to draft: false.
 
 # Expected output will be something like:
 #
 # ---
+# author: Sam White
+# toc-title: Contents
+# toc-depth: 5
+# toc-location: left
 # layout: post
-# title: First post test
-# date: '2018-10-29'
-# tags:
-#   - temporary
-# categories:
-#   - delete
+# title: qPCR - C.gigas PolyIC Diploid MgCl2
+# date: '2023-08-17 06:38'
+# tags: 
+#   - qPCR
+#   - CFX Connect
+#   - Crassostrea gigas
+#   - Pacific oyster
+#   - polyIC
+# categories: 
+#   - 2023
+#   - Miscellaneous
+# draft: true
 # ---
 
 
 # To run, copy this file to your desired directory.
 # Change to the directory where you just copied this file.
-# In a terminal prompt, type:. jekyll_header.sh
+# In a terminal prompt, type:. quarto_yaml.sh
 
 # Set variables
+author="author: Sam White"
 post_date=$(date '+%Y-%m-%d')
 md_line="---"
-layout="layout: post"
 title="title: "
 date_line="date: "
-comments="comments: true"
-tags="tags: "
 categories="categories: "
 
 # Ask user for input
 echo "Enter post title (use no punctuation):"
 read post_title
-
-echo "Enter tags (space separated)"
-read -a tag_array
 
 echo "Enter categories (space separated)"
 read -a categories_array
@@ -51,18 +54,29 @@ read -a categories_array
 # remove spaces from post-title and replace with hyphens
 formatted_title=$(echo -ne "${post_title}" | tr [:space:] '-')
 
-# save new filename using post_date and formatted_phrase variables.
-new_md_file="$(echo -n "${post_date}"-"${formatted_title}")".md
+# save new filename using post_date and formatted_title variables.
+mkdir --parents "${post_date}-${formatted_title}" && cd $_
 
 
-# prints formatted jekyll header utilizing post_date and user-entered post title and tags/categories.
-# Tags and categories are formatted as Jekyll array - needed for Basically Basic theme.
-# writes contents to new_md_file
-printf "%s\n" "${md_line}" "${layout}" "${title}${post_title}" "${date_line}'${post_date}'" "${tags}" >> "${new_md_file}"
-printf "  - %s\n" "${tag_array[@]}" >> "${new_md_file}"
-printf "%s\n" "${categories}" >> "${new_md_file}"
-printf "  - %s\n" "${categories_array[@]}" >> "${new_md_file}"
-printf "%s\n" "${md_line}" >> "${new_md_file}"
+# prints formatted Quarto YAML header utilizing post_date and user-entered post title and categories.
+# writes contents to index.qmd
+printf "%s\n" \
+"${md_line}" \
+"${author}" \
+"toc-title: Contents" \
+"toc-depth: 5" \
+"toc-location: left" \
+"${title}${post_title}"
+"${date_line}'${post_date}'" \
+>> index.qmd
+
+printf "%s\n" "${categories}" >> index.qmd
+
+printf "  - %s\n" "${categories_array[@]}" >> index.qmd
+
+printf "%s\n" "draft: true"
+
+printf "%s\n" "${md_line}" >> index.qmd
 
 # Open file with Atom text editor.
-atom "${new_md_file}"
+atom index.qmd
