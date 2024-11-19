1162ef1d201393b5b49c6edb434b70ee5ef09c1a
kubu4 <samuel.j.white@gmail.com>
2024-11-19 08:31:12 -0800

    set absoluate path to command script in 20241118-cvir-bismark-bowtie2-alignments.sh

--- sbatch_scripts/20241118-cvir-bismark-bowtie2-alignments.sh
+++ sbatch_scripts/20241118-cvir-bismark-bowtie2-alignments.sh
@@ -24,4 +24,4 @@ apptainer exec \
 --bind /mmfs1/home/ \
 --bind /gscratch \
 /gscratch/srlab/sr320/srlab-bioinformatics-container-586bf21.sif \
-./20241119-cvir-bismark-bowtie2-alignments.sh
+/mmfs1/home/samwhite/gitrepos/RobertsLab/sams-notebook/sbatch_scripts/20241119-cvir-bismark-bowtie2-alignments.sh
