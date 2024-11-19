d2181eb7a66c1a72a55ed6860a42cf2c9bb9d87f
kubu4 <samuel.j.white@gmail.com>
2024-11-19 07:56:59 -0800

    replace bismark commands with script call in 20241118-cvir-bismark-bowtie2-alignments.sh

--- sbatch_scripts/20241118-cvir-bismark-bowtie2-alignments.sh
+++ sbatch_scripts/20241118-cvir-bismark-bowtie2-alignments.sh
@@ -15,53 +15,13 @@
 ## Specify the working directory for this job
 #SBATCH --chdir=/gscratch/scrubbed/samwhite/gitrepos/ceasmallr/output/02.00-bismark-bowtie2-alignment/
 
-###################################################################################
-# These variables need to be set by user
-
-## Assign Variables
-
-# INPUT FILES
-repo_dir="/gscratch/scrubbed/samwhite/gitrepos/ceasmallr"
-trimmed_fastqs_dir="${repo_dir}/output/00.00-trimming-fastp"
-bisulfite_genome_dir="${repo_dir}/data/Cvirginica_v300"
-
-
-# OUTPUT FILES
-output_dir_top="${repo_dir}/output/02.00-bismark-bowtie2-alignment"
-
-# PARAMETERS
-bowtie2_min_score="L,0,-0.6"
-
-# Program paths
-samtools_dir=
-bowtie2_dir=
-bismark_dir=
-bisulfite_genome_dir=
-
-# CPU threads
-# Bismark already spawns multiple instances and additional threads are multiplicative."
-bismark_threads=8
-
-
-###################################################################################
-
-# Get the FastQ file pair for this task
-pair=$(sed -n "${SLURM_ARRAY_TASK_ID}p" fastq_pairs.txt)
-R1=$(echo $pair | awk '{print $1}')
-R2=$(echo $pair | awk '{print $2}')
-
-# Get just the sample name (excludes the _R[12]_001*)
-sample_name="${R1%%_*}"
-
-# Run Bismark
-${bismark_dir}/bismark \
---genome ${bisulfite_genome_dir} \
---score_min "${bowtie2_min_score}" \
---parallel "${bismark_threads}" \
---non_directional \
---gzip \
--p "${threads}" \
--1 ${trimmed_fastqs_dir}/${R1}\
--2 ${trimmed_fastqs_dir}/${R2} \
---output_dir "${output_dir_top}" \
-2> "${output_dir_top}"/${sample_name}-{SLURM_ARRAY_TASK_ID}-bismark_summary.txt
+# Execute Roberts Lab bioinformatics container
+# Binds home directory
+# Binds /gscratch directory
+# Directory bindings allow outputs to be written to the hard drive.
+apptainer exec \
+--home "$PWD" \
+--bind /mmfs1/home/ \
+--bind /gscratch \
+/gscratch/srlab/sr320/srlab-bioinformatics-container-586bf21.sif \
+./20241119-cvir-bismark-bowtie2-alignments.sh
